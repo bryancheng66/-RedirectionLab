ea94f1738745fe569ecbf939f30267daf4d74128
bryancheng66 <bryancheng66@gmail.com>
2020-12-31 14:06:58 -0500

    make pigLatin

--- PigLatin.java
+++ PigLatin.java
@@ -3,11 +3,12 @@ import java.util.Arrays;
 public class PigLatin {
 	public static String[] vowels = {"a", "e", "i", "o", "u"};
 
-	public static boolean startsVowel(String s){
+	public static String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+
+	public static boolean startsString(String s, String[] array){
 		boolean result = false;
 		for (int i = 0; !result && i < vowels.length; i++){
-			System.out.println(s.substring(0,1));
-			if (vowels[i].equals(s.substring(0,1))){
+			if (array[i].equals(s.substring(0, array[0].length()))){
 				result = true;
 			}
 		}
@@ -17,8 +18,21 @@ public class PigLatin {
 	public static String pigLatinSimple(String s){
 		String result = "";
 
-		if (startsVowel(s)){
+		if (startsString(s, vowels)){
 			result += s + "hay";
+		} else {
+			result += s.substring(1) + s.substring(0,1) + "ay";
+		}
+		return result;
+	}
+
+	public static String pigLatin(String s){
+		String result = "";
+		
+		if (startsString(s, vowels)){
+			result += s + "hay";
+		} else if (startsString(s, digraphs)){
+			result += s.substring(2) + s.substring(0,2) + "ay";
 		} else {
 			result += s.substring(1) + s.substring(0,1) + "ay";
 		}
@@ -27,5 +41,6 @@ public class PigLatin {
 
 	public static void main(String[] Args){
 		System.out.println(pigLatinSimple(Args[0]));
+		System.out.println(pigLatin(Args[1]));
 	}
 }
