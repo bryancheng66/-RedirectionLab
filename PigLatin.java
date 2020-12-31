252808601879c41dab73e49667c9868df66b94a2
bryancheng66 <bryancheng66@gmail.com>
2020-12-31 13:40:34 -0500

    fix pigLatinSimple

--- PigLatin.java
+++ PigLatin.java
@@ -3,17 +3,21 @@ import java.util.Arrays;
 public class PigLatin {
 	public static String[] vowels = {"a", "e", "i", "o", "u"};
 
-	public static String pigLatinSimple(String s){
-		boolean startsVowels = false;
-		for (int i = 0; !startsVowels && i < vowels.length; i++){
-			if (vowels[i] == s.substring(0,1)){
-				startsVowels = true;
+	public static boolean startsVowel(String s){
+		boolean result = false;
+		for (int i = 0; !result && i < vowels.length; i++){
+			System.out.println(s.substring(0,1));
+			if (vowels[i].equals(s.substring(0,1))){
+				result = true;
 			}
 		}
-		
+		return result;
+	}
+
+	public static String pigLatinSimple(String s){
 		String result = "";
 
-		if (startsVowels){
+		if (startsVowel(s)){
 			result += s + "hay";
 		} else {
 			result += s.substring(1) + s.substring(0,1) + "ay";
